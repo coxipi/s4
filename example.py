f1632341a9ee47cb164d579ee9bfc8787b31ed7e
Éric Dupuis <eric.dupuis.1@umontreal.ca>
2025-04-08 21:36:22 -0400

    deep-rnn mnist test

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-'''
+"""
 Train an S4 model on sequential CIFAR10 / sequential MNIST with PyTorch for demonstration purposes.
 This code borrows heavily from https://github.com/kuangliu/pytorch-cifar.
 
@@ -20,65 +20,98 @@ The default CIFAR10 model trained by this file should get
 89+% accuracy on the CIFAR10 test set in 80 epochs.
 
 Each epoch takes approximately 7m20s on a T4 GPU (will be much faster on V100 / A100).
-'''
+"""
+import argparse
+import os
+import random
+
+import numpy as np
 import torch
+import torch.backends.cudnn as cudnn
 import torch.nn as nn
 import torch.optim as optim
-import torch.backends.cudnn as cudnn
-
 import torchvision
 import torchvision.transforms as transforms
-
-import os
-import argparse
-
-from models.s4.s4 import S4Block as S4  # Can use full version instead of minimal S4D standalone below
 from models.s4.s4d import S4D
 from tqdm.auto import tqdm
 
+from models.s4.s4 import (
+    S4Block as S4,  # Can use full version instead of minimal S4D standalone below
+)
+
 # Dropout broke in PyTorch 1.11
-if tuple(map(int, torch.__version__.split('.')[:2])) == (1, 11):
+if tuple(map(int, torch.__version__.split(".")[:2])) == (1, 11):
     print("WARNING: Dropout is bugged in PyTorch 1.11. Results may be worse.")
     dropout_fn = nn.Dropout
-if tuple(map(int, torch.__version__.split('.')[:2])) >= (1, 12):
+if tuple(map(int, torch.__version__.split(".")[:2])) >= (1, 12):
     dropout_fn = nn.Dropout1d
 else:
     dropout_fn = nn.Dropout2d
 
 
-
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser = argparse.ArgumentParser(description="PyTorch CIFAR10 Training")
+# Seed
+parser.add_argument("--seed", default=1, type=int, help="Seed randomness.")
+# Model
+parser.add_argument(
+    "--model", default="S4", type=str, choices=["S4", "DeepRNN"], help="Model"
+)
 # Optimizer
-parser.add_argument('--lr', default=0.01, type=float, help='Learning rate')
-parser.add_argument('--weight_decay', default=0.01, type=float, help='Weight decay')
+parser.add_argument("--lr", default=0.01, type=float, help="Learning rate")
+parser.add_argument("--weight_decay", default=0.01, type=float, help="Weight decay")
 # Scheduler
 # parser.add_argument('--patience', default=10, type=float, help='Patience for learning rate scheduler')
-parser.add_argument('--epochs', default=100, type=float, help='Training epochs')
+parser.add_argument("--epochs", default=100, type=float, help="Training epochs")
 # Dataset
-parser.add_argument('--dataset', default='cifar10', choices=['mnist', 'cifar10'], type=str, help='Dataset')
-parser.add_argument('--grayscale', action='store_true', help='Use grayscale CIFAR10')
+parser.add_argument(
+    "--dataset",
+    default="cifar10",
+    choices=["mnist", "cifar10"],
+    type=str,
+    help="Dataset",
+)
+parser.add_argument("--grayscale", action="store_true", help="Use grayscale CIFAR10")
 # Dataloader
-parser.add_argument('--num_workers', default=4, type=int, help='Number of workers to use for dataloader')
-parser.add_argument('--batch_size', default=64, type=int, help='Batch size')
+parser.add_argument(
+    "--num_workers", default=4, type=int, help="Number of workers to use for dataloader"
+)
+parser.add_argument("--batch_size", default=64, type=int, help="Batch size")
 # Model
-parser.add_argument('--n_layers', default=4, type=int, help='Number of layers')
-parser.add_argument('--d_model', default=128, type=int, help='Model dimension')
-parser.add_argument('--dropout', default=0.1, type=float, help='Dropout')
-parser.add_argument('--prenorm', action='store_true', help='Prenorm')
+parser.add_argument("--n_layers", default=4, type=int, help="Number of layers")
+parser.add_argument("--d_model", default=128, type=int, help="Model dimension")
+parser.add_argument("--dropout", default=0.1, type=float, help="Dropout")
+# not implemented with torch's basic deep-RNN
+parser.add_argument("--prenorm", action="store_true", help="Prenorm")
 # General
-parser.add_argument('--resume', '-r', action='store_true', help='Resume from checkpoint')
+parser.add_argument(
+    "--resume", "-r", action="store_true", help="Resume from checkpoint"
+)
 
 args = parser.parse_args()
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = "cuda" if torch.cuda.is_available() else "cpu"
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
+
+# Seeds
+# There's a manual seed below in `split_train_val`, set at 42. For now I won't touch it.
+def seed_everything(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+seed_everything(args.seed)
 # Data
-print(f'==> Preparing {args.dataset} data..')
+print(f"==> Preparing {args.dataset} data..")
+
 
 def split_train_val(train, val_split):
-    train_len = int(len(train) * (1.0-val_split))
+    train_len = int(len(train) * (1.0 - val_split))
     train, val = torch.utils.data.random_split(
         train,
         (train_len, len(train) - train_len),
@@ -86,72 +119,86 @@ def split_train_val(train, val_split):
     )
     return train, val
 
-if args.dataset == 'cifar10':
 
+if args.dataset == "cifar10":
     if args.grayscale:
-        transform = transforms.Compose([
-            transforms.Grayscale(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=122.6 / 255.0, std=61.0 / 255.0),
-            transforms.Lambda(lambda x: x.view(1, 1024).t())
-        ])
+        transform = transforms.Compose(
+            [
+                transforms.Grayscale(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=122.6 / 255.0, std=61.0 / 255.0),
+                transforms.Lambda(lambda x: x.view(1, 1024).t()),
+            ]
+        )
     else:
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-            transforms.Lambda(lambda x: x.view(3, 1024).t())
-        ])
+        transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                ),
+                transforms.Lambda(lambda x: x.view(3, 1024).t()),
+            ]
+        )
 
     # S4 is trained on sequences with no data augmentation!
     transform_train = transform_test = transform
 
     trainset = torchvision.datasets.CIFAR10(
-        root='./data/cifar/', train=True, download=True, transform=transform_train)
+        root="./data/cifar/", train=True, download=True, transform=transform_train
+    )
     trainset, _ = split_train_val(trainset, val_split=0.1)
 
     valset = torchvision.datasets.CIFAR10(
-        root='./data/cifar/', train=True, download=True, transform=transform_test)
+        root="./data/cifar/", train=True, download=True, transform=transform_test
+    )
     _, valset = split_train_val(valset, val_split=0.1)
 
     testset = torchvision.datasets.CIFAR10(
-        root='./data/cifar/', train=False, download=True, transform=transform_test)
+        root="./data/cifar/", train=False, download=True, transform=transform_test
+    )
 
     d_input = 3 if not args.grayscale else 1
     d_output = 10
 
-elif args.dataset == 'mnist':
-
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Lambda(lambda x: x.view(1, 784).t())
-    ])
+elif args.dataset == "mnist":
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Lambda(lambda x: x.view(1, 784).t())]
+    )
     transform_train = transform_test = transform
 
     trainset = torchvision.datasets.MNIST(
-        root='./data', train=True, download=True, transform=transform_train)
+        root="./data", train=True, download=True, transform=transform_train
+    )
     trainset, _ = split_train_val(trainset, val_split=0.1)
 
     valset = torchvision.datasets.MNIST(
-        root='./data', train=True, download=True, transform=transform_test)
+        root="./data", train=True, download=True, transform=transform_test
+    )
     _, valset = split_train_val(valset, val_split=0.1)
 
     testset = torchvision.datasets.MNIST(
-        root='./data', train=False, download=True, transform=transform_test)
+        root="./data", train=False, download=True, transform=transform_test
+    )
 
     d_input = 1
     d_output = 10
-else: raise NotImplementedError
+else:
+    raise NotImplementedError
 
 # Dataloaders
 trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
+    trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers
+)
 valloader = torch.utils.data.DataLoader(
-    valset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+    valset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers
+)
 testloader = torch.utils.data.DataLoader(
-    testset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
+    testset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers
+)
+
 
 class S4Model(nn.Module):
-
     def __init__(
         self,
         d_input,
@@ -220,29 +267,154 @@ class S4Model(nn.Module):
 
         return x
 
-# Model
-print('==> Building model..')
-model = S4Model(
-    d_input=d_input,
-    d_output=d_output,
-    d_model=args.d_model,
-    n_layers=args.n_layers,
-    dropout=args.dropout,
-    prenorm=args.prenorm,
-)
 
+# INCOMPLETE
+# Would this be interesting to implement?
+# class RNNModel_manual(nn.Module):
+#     """This reproduces the manual stacking of S4 and in the deep-linear-rnn
+#     repo. This is not necessary, but maybe it will be useful. For instance,
+#     I'm not sure that RNN in torch does this "residual connection", and it
+#     doesn't allow norm between layers (AFAIU so far)
+#     """
+#     def __init__(
+#         self,
+#         d_input,
+#         d_output=10,
+#         d_model=256,
+#         n_layers=4,
+#         dropout=0.2,
+#         prenorm=False,
+#         rnn_type='RNN'
+#     ):
+#         super().__init__()
+
+#         self.prenorm = prenorm
+#         self.rnn_type = rnn_type.upper()
+
+#         # Linear encoder (e.g., maps 1 → d_model for grayscale)
+#         self.encoder = nn.Linear(d_input, d_model)
+
+#         # Norm + Dropout for each layer
+#         self.norms = nn.ModuleList([nn.LayerNorm(d_model) for _ in range(n_layers)])
+#         self.dropouts = nn.ModuleList([nn.Dropout(dropout) for _ in range(n_layers)])
+
+#         # Use RNN/GRU/LSTM layers stacked manually
+#         rnn_cls = {
+#             'RNN': nn.RNN,
+#             'LSTM': nn.LSTM,
+#             'GRU': nn.GRU
+#         }.get(self.rnn_type)
+
+#         if rnn_cls is None:
+#             raise ValueError(f"Unsupported rnn_type: {rnn_type}")
+
+#         self.rnn_layers = nn.ModuleList([
+#             rnn_cls(input_size=d_model, hidden_size=d_model, num_layers=1, batch_first=True)
+#             for _ in range(n_layers)
+#         ])
+
+#         # Decoder
+#         self.decoder = nn.Linear(d_model, d_output)
+
+#     def forward(self, x):
+#         """
+#         Input x is shape (B, L, d_input)
+#         """
+#         x = self.encoder(x)  # (B, L, d_input) -> (B, L, d_model)
+
+#         for rnn, norm, dropout in zip(self.rnn_layers, self.norms, self.dropouts):
+#             z = x
+#             if self.prenorm:
+#                 z = norm(z)
+
+#             z, _ = rnn(z)  # (B, L, d_model)
+#             z = dropout(z)
+
+#             # Residual connection
+#             x = z + x
+
+#             if not self.prenorm:
+#                 x = norm(x)
+
+#         # Pooling (average over sequence length)
+#         x = x.mean(dim=1)  # (B, d_model)
+
+#         return self.decoder(x)  # (B, d_output)
+
+
+class RNNModel(nn.Module):
+    def __init__(
+        self,
+        d_input,
+        d_output=10,
+        d_model=256,
+        n_layers=4,
+        dropout=0.2,
+    ):
+        super().__init__()
+
+        self.encoder = nn.Linear(d_input, d_model)
+
+        self.rnn = nn.RNN(
+            input_size=d_model,
+            hidden_size=d_model,
+            num_layers=n_layers,
+            dropout=dropout if n_layers > 1 else 0.0,
+            batch_first=True,
+        )
+
+        self.decoder = nn.Linear(d_model, d_output)
+
+    def forward(self, x):
+        """
+        Input: x of shape (B, L, d_input)
+        Output: (B, d_output)
+        """
+        x = self.encoder(x)  # (B, L, d_input) -> (B, L, d_model)
+        x, _ = self.rnn(x)  # (B, L, d_model) -> (B, L, d_model)
+        # Instead of having dim(y) = dim(x), we now pool, as in S4
+        # Our task is classification and not copy, makes sense
+        x = x.mean(dim=1)  # (B, d_model) via average pooling over sequence
+        x = self.decoder(x)  # (B, d_model) -> (B, d_output)
+        return x
+
+
+# Model
+print("==> Building model..")
+if args.model == "S4":
+    model = S4Model(
+        d_input=d_input,
+        d_output=d_output,
+        d_model=args.d_model,
+        n_layers=args.n_layers,
+        dropout=args.dropout,
+        prenorm=args.prenorm,
+    )
+elif args.model == "DeepRNN":
+    model = RNNModel(
+        d_input=d_input,
+        d_output=d_output,
+        d_model=args.d_model,
+        n_layers=args.n_layers,
+        dropout=args.dropout,
+        # I believe `prenorm` is not possible with torch's builtin implementation
+        # prenorm=args.prenorm,
+    )
+else:
+    raise ValueError(f"Unsupported model: {args.model}. Give either 'S4' or 'DeepRNN'.")
 model = model.to(device)
-if device == 'cuda':
+if device == "cuda":
     cudnn.benchmark = True
 
 if args.resume:
     # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
-    model.load_state_dict(checkpoint['model'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
+    print("==> Resuming from checkpoint..")
+    assert os.path.isdir("checkpoint"), "Error: no checkpoint directory found!"
+    checkpoint = torch.load("./checkpoint/ckpt.pth")
+    model.load_state_dict(checkpoint["model"])
+    best_acc = checkpoint["acc"]
+    start_epoch = checkpoint["epoch"]
+
 
 def setup_optimizer(model, lr, weight_decay, epochs):
     """
@@ -267,13 +439,12 @@ def setup_optimizer(model, lr, weight_decay, epochs):
     # Add parameters with special hyperparameters
     hps = [getattr(p, "_optim") for p in all_parameters if hasattr(p, "_optim")]
     hps = [
-        dict(s) for s in sorted(list(dict.fromkeys(frozenset(hp.items()) for hp in hps)))
+        dict(s)
+        for s in sorted(list(dict.fromkeys(frozenset(hp.items()) for hp in hps)))
     ]  # Unique dicts
     for hp in hps:
         params = [p for p in all_parameters if getattr(p, "_optim", None) == hp]
-        optimizer.add_param_group(
-            {"params": params, **hp}
-        )
+        optimizer.add_param_group({"params": params, **hp})
 
     # Create a lr scheduler
     # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=patience, factor=0.2)
@@ -283,12 +454,18 @@ def setup_optimizer(model, lr, weight_decay, epochs):
     keys = sorted(set([k for hp in hps for k in hp.keys()]))
     for i, g in enumerate(optimizer.param_groups):
         group_hps = {k: g.get(k, None) for k in keys}
-        print(' | '.join([
-            f"Optimizer group {i}",
-            f"{len(g['params'])} tensors",
-        ] + [f"{k} {v}" for k, v in group_hps.items()]))
+        print(
+            " | ".join(
+                [
+                    f"Optimizer group {i}",
+                    f"{len(g['params'])} tensors",
+                ]
+                + [f"{k} {v}" for k, v in group_hps.items()]
+            )
+        )
 
     return optimizer, scheduler
+
 
 criterion = nn.CrossEntropyLoss()
 optimizer, scheduler = setup_optimizer(
@@ -298,6 +475,7 @@ optimizer, scheduler = setup_optimizer(
 ###############################################################################
 # Everything after this point is standard PyTorch training!
 ###############################################################################
+
 
 # Training
 def train():
@@ -320,8 +498,15 @@ def train():
         correct += predicted.eq(targets).sum().item()
 
         pbar.set_description(
-            'Batch Idx: (%d/%d) | Loss: %.3f | Acc: %.3f%% (%d/%d)' %
-            (batch_idx, len(trainloader), train_loss/(batch_idx+1), 100.*correct/total, correct, total)
+            "Batch Idx: (%d/%d) | Loss: %.3f | Acc: %.3f%% (%d/%d)"
+            % (
+                batch_idx,
+                len(trainloader),
+                train_loss / (batch_idx + 1),
+                100.0 * correct / total,
+                correct,
+                total,
+            )
         )
 
 
@@ -344,35 +529,42 @@ def eval(epoch, dataloader, checkpoint=False):
             correct += predicted.eq(targets).sum().item()
 
             pbar.set_description(
-                'Batch Idx: (%d/%d) | Loss: %.3f | Acc: %.3f%% (%d/%d)' %
-                (batch_idx, len(dataloader), eval_loss/(batch_idx+1), 100.*correct/total, correct, total)
+                "Batch Idx: (%d/%d) | Loss: %.3f | Acc: %.3f%% (%d/%d)"
+                % (
+                    batch_idx,
+                    len(dataloader),
+                    eval_loss / (batch_idx + 1),
+                    100.0 * correct / total,
+                    correct,
+                    total,
+                )
             )
 
     # Save checkpoint.
     if checkpoint:
-        acc = 100.*correct/total
+        acc = 100.0 * correct / total
         if acc > best_acc:
             state = {
-                'model': model.state_dict(),
-                'acc': acc,
-                'epoch': epoch,
+                "model": model.state_dict(),
+                "acc": acc,
+                "epoch": epoch,
             }
-            if not os.path.isdir('checkpoint'):
-                os.mkdir('checkpoint')
-            torch.save(state, './checkpoint/ckpt.pth')
+            if not os.path.isdir("checkpoint"):
+                os.mkdir("checkpoint")
+            torch.save(state, "./checkpoint/ckpt.pth")
             best_acc = acc
 
         return acc
 
+
 pbar = tqdm(range(start_epoch, args.epochs))
 for epoch in pbar:
     if epoch == 0:
-        pbar.set_description('Epoch: %d' % (epoch))
+        pbar.set_description("Epoch: %d" % (epoch))
     else:
-        pbar.set_description('Epoch: %d | Val acc: %1.3f' % (epoch, val_acc))
+        pbar.set_description("Epoch: %d | Val acc: %1.3f" % (epoch, val_acc))
     train()
     val_acc = eval(epoch, valloader, checkpoint=True)
     eval(epoch, testloader)
     scheduler.step()
     # print(f"Epoch {epoch} learning rate: {scheduler.get_last_lr()}")
-

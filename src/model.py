4fdf0cc00b850ae907255838f94f608cf3e674be
Éric Dupuis <eric.dupuis.1@umontreal.ca>
2025-04-14 18:17:45 -0400

    add more general S4 && new S4DModel

--- src/model.py
+++ src/model.py
@@ -33,11 +33,14 @@ class S4Model(nn.Module):
         dropout=0.1, # changed the default value in the class to reflect the default value in the script
         prenorm=False,
         lr=0.01, 
+        mode = 'dplr'
     ):
         super(S4Model, self).__init__()
 
         self.prenorm = prenorm
         self.N = N
+        self.lr = lr 
+        self.mode = mode
 
         # Linear encoder (d_input = 1 for grayscale and 3 for RGB)
         self.encoder = nn.Linear(d_input, d_model)
@@ -48,7 +51,81 @@ class S4Model(nn.Module):
         self.dropouts = nn.ModuleList()
         for _ in range(n_layers):
             self.s4_layers.append(
-                S4D(d_model, dropout=dropout, d_state=self.N, transposed=True, lr=min(0.001,0.01))
+                S4(d_model, dropout=dropout, d_state=self.N, transposed=True, lr=min(0.001, self.lr), mode=self.mode)
+            )
+            self.norms.append(nn.LayerNorm(d_model))
+            self.dropouts.append(dropout_fn(dropout))
+
+        # Linear decoder
+        self.decoder = nn.Linear(d_model, d_output)
+
+    def forward(self, x):
+        """
+        Input x is shape (B, L, d_input)
+        """
+        x = self.encoder(x)  # (B, L, d_input) -> (B, L, d_model)
+
+        x = x.transpose(-1, -2)  # (B, L, d_model) -> (B, d_model, L)
+        for layer, norm, dropout in zip(self.s4_layers, self.norms, self.dropouts):
+            # Each iteration of this loop will map (B, d_model, L) -> (B, d_model, L)
+
+            z = x
+            if self.prenorm:
+                # Prenorm
+                z = norm(z.transpose(-1, -2)).transpose(-1, -2)
+
+            # Apply S4 block: we ignore the state input and output
+            z, _ = layer(z)
+
+            # Dropout on the output of the S4 block
+            z = dropout(z)
+
+            # Residual connection
+            x = z + x
+
+            if not self.prenorm:
+                # Postnorm
+                x = norm(x.transpose(-1, -2)).transpose(-1, -2)
+
+        x = x.transpose(-1, -2)
+
+        # Pooling: average pooling over the sequence length
+        x = x.mean(dim=1)
+
+        # Decode the outputs
+        x = self.decoder(x)  # (B, d_model) -> (B, d_output)
+
+        return x
+
+# not sure if
+class S4DModel(nn.Module):
+    def __init__(
+        self,
+        d_input,
+        d_output=10,
+        d_model=256,
+        N = 64,
+        n_layers=4,
+        dropout=0.1, # changed the default value in the class to reflect the default value in the script
+        prenorm=False,
+        lr=0.01, 
+    ):
+        super(S4DModel, self).__init__()
+
+        self.prenorm = prenorm
+        self.N = N
+        self.lr = lr 
+
+        # Linear encoder (d_input = 1 for grayscale and 3 for RGB)
+        self.encoder = nn.Linear(d_input, d_model)
+
+        # Stack S4 layers as residual blocks
+        self.s4_layers = nn.ModuleList()
+        self.norms = nn.ModuleList()
+        self.dropouts = nn.ModuleList()
+        for _ in range(n_layers):
+            self.s4_layers.append(
+                S4D(d_model, dropout=dropout, d_state=self.N, transposed=True, lr=min(0.001, self.lr))
             )
             self.norms.append(nn.LayerNorm(d_model))
             self.dropouts.append(dropout_fn(dropout))
